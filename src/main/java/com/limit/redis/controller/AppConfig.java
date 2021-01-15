a532ee4147d6796a0cfcb563cd2168a2ae1b03be
oleg <gusevoleg96@gmail.ru>
2021-01-15 17:24:12 +0300

    Test

---
+++ src/main/java/com/limit/redis/controller/AppConfig.java
@@ -0,0 +1,17 @@
+package com.limit.redis.controller;
+
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AppConfig {
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
+    }
+}
